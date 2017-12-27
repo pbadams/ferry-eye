7a4ad4b411ea6d23e3c113c019ae0e164375ef77
Paul Adams <pauladams@pingidentity.com>
2017-12-27 15:30:09 -0800

    Added HTML tidy to clean-up poor formatting

--- archive-ferry.sh
+++ archive-ferry.sh
@@ -6,3 +6,5 @@ curl \
 -H "Connection: close" \
 -o $DIR/HSB-Route03-$DATE.html \
 'http://orca.bcferries.com:8080/cc/marqui/arrivals-departures.asp?DEPT=HSB&route=03' 
+
+tidy -q --vertical-space no -i -o $DIR/HSB-Route03-$DATE.CLEAN.html $DIR/HSB-Route03-$DATE.html 
