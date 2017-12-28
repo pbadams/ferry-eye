9ea4b8666d3785ba4a40a667bd9514902ea26a0f
Paul Adams <pauladams@pingidentity.com>
2017-12-28 08:20:10 -0800

    Create only one CELAN file per day (overwrite with tidy version)

--- archive-ferry.sh
+++ archive-ferry.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 DIR=$1
-DATE=$(date +%Y-%m-%d_%H-%M-%S)
+TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
+DATE=$(date +%Y-%m-%d)
 
 curl \
 -H "Connection: close" \
--o $DIR/HSB-Route03-$DATE.html \
+-o $DIR/HSB-Route03-$TIMESTAMP.html \
 'http://orca.bcferries.com:8080/cc/marqui/arrivals-departures.asp?DEPT=HSB&route=03' 
 
-tidy -q --vertical-space no -i -o $DIR/HSB-Route03-$DATE.CLEAN.html $DIR/HSB-Route03-$DATE.html 
+tidy -q --vertical-space no -c -asxhtml -i -o $DIR/HSB-Route03-$DATE.CLEAN.html $DIR/HSB-Route03-$TIMESTAMP.html 
+ 
